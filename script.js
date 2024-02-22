adfd757acb244ba96fe5532681e1b492821e1013
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-22 16:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function maj(n)
+var ct=0;
+var ctt=0;
+var ind=0;
+var ii=0;
+for(var i=0;i<n.length;i++){
+	for(var j=i;j<n.length.j++){
+		if(n[i]==n[j]){
+			ct++
+            ind=i;
+		}
+		}
+		if(ct>ctt){
+			ctt=ct;
+			 ii=ind;
+	}
+} 
+  
+var flor=Math.floor(n.length/2);
+
+	return ctt;
+}
